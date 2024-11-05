7d2efbb2329b43ea39d2b82c45db5bf2717912ac
Adarsh <adarsh.a@cloudnautical.com>
2024-11-05 14:12:13 +0530

    install apps

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Custom apps
+    "userauths",
+    "store",
+    "vendor",
+    "customer",
+    "api",
 ]
 
 MIDDLEWARE = [
